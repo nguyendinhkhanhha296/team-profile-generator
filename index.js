805e39e1e29fc74fd72335787d80a44fffd00114
nguyendinhkhanhha296 <nguyendinhkhanhha296@gmail.com>
2022-08-23 12:09:33 -0700

    fix folder

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Intern = require("./lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output");
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const generateTeam = require("./src/template.js")
 
